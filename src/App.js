83904efa195b794064ab5c9d3497579b54aea3d8
Ethan Lieberman <ethan.lieberman@yahoo.com>
2022-05-27 16:46:14 -0700

    general structure setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom'
+import { useState } from 'react';
+import Home from './view/Home';
+import Footer from './components/Footer';
+
 
 function App() {
+
+  const [darkmode, setDarkmode] = useState(false)
+
+  const modeSelect = () => {    // function called from the button in the header componant to set dark/light modes across app
+    darkmode == false ?    // ternary to check the current boolean and reverse it when function is called
+      setDarkmode(true)
+      :
+      setDarkmode(false)
+
+    console.log('darkmode is set to', darkmode)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+      <Switch>
+
+        <Route path={'/'}>
+          <Home darkmode={darkmode} />
+        </Route>
+
+        <Route path={'/projects'}>
+          <Projects/>
+        </Route>
+
+
+      </Switch>
+
+      <Footer/>
+
     </div>
   );
 }
